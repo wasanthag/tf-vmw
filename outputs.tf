0bbecaef01a3522c77237f9a883d8774fe13f556
Wasantha Gamage <wasanthag1302@gmail.com>
2020-06-08 16:21:26 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "App1_Server_Public_IP" {
   value = vsphere_virtual_machine.app1.default_ip_address
 }
 # windows Server IP
-output "Windows_Server_Public_IP" {
- value = vsphere_virtual_machine.win2019-vm.default_ip_address
-}
+#output "Windows_Server_Public_IP" {
+# value = vsphere_virtual_machine.win2019-vm.default_ip_address
+#}
 
